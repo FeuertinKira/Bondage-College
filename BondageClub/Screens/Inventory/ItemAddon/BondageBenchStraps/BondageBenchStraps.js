317ba8190934f72f90bb5f938f4e239aff28020a
EveFerox <eveferox@gmail.com>
2019-11-08 00:22:02 +0200

    fix

--- BondageClub/Screens/Inventory/ItemAddon/BondageBenchStraps/BondageBenchStraps.js
+++ BondageClub/Screens/Inventory/ItemAddon/BondageBenchStraps/BondageBenchStraps.js
@@ -1,14 +1,14 @@
 "use strict";
-var InventoryItemAddonBondageBenchStrapsMessage = "";
+var InventoryItemMiscBondageBenchStrapsMessage = "";
 
 // Loads the item extension properties
-function InventoryItemAddonBondageBenchStrapsLoad() {
+function InventoryItemMiscBondageBenchStrapsLoad() {
 	if (DialogFocusItem.Property == null) DialogFocusItem.Property = { Restrain: null };
 	DialogFocusItem.Property.SelfUnlock = false;
 }
 
 // Draw the item extension screen
-function InventoryItemAddonBondageBenchStrapsDraw() {
+function InventoryItemMiscBondageBenchStrapsDraw() {
 	
 	// Draw the header and item
 	DrawRect(1387, 125, 225, 275, "white");
@@ -31,40 +31,41 @@ function InventoryItemAddonBondageBenchStrapsDraw() {
 	DrawText(DialogFind(Player, "BondageBenchStrapsPoseFull"), 1875, 800, "white", "gray");
 
 	// Draw the message if present
-	if (InventoryItemAddonBondageBenchStrapsMessage != null) DrawTextWrap(DialogFind(Player, InventoryItemAddonBondageBenchStrapsMessage), 1100, 850, 800, 160, "White");
+	if (InventoryItemMiscBondageBenchStrapsMessage != null) DrawTextWrap(DialogFind(Player, InventoryItemMiscBondageBenchStrapsMessage), 1100, 850, 800, 160, "White");
 }
 
 // Catches the item extension clicks
-function InventoryItemAddonBondageBenchStrapsClick() {
+function InventoryItemMiscBondageBenchStrapsClick() {
 	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if (CommonIsClickAt(1000, 550, 225, 225) && (DialogFocusItem.Property.Restrain != null)) InventoryItemAddonBondageBenchStrapsSetPose(null);
-	if (CommonIsClickAt(1250, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Normal"))) InventoryItemAddonBondageBenchStrapsSetPose("Normal");
-	if (CommonIsClickAt(1500, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Heavy"))) InventoryItemAddonBondageBenchStrapsSetPose("Heavy");
-	if (CommonIsClickAt(1750, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Full"))) InventoryItemAddonBondageBenchStrapsSetPose("Full");
+	if (CommonIsClickAt(1000, 550, 225, 225) && (DialogFocusItem.Property.Restrain != null)) InventoryItemMiscBondageBenchStrapsSetPose(null);
+	if (CommonIsClickAt(1250, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Normal"))) InventoryItemMiscBondageBenchStrapsSetPose("Normal");
+	if (CommonIsClickAt(1500, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Heavy"))) InventoryItemMiscBondageBenchStrapsSetPose("Heavy");
+	if (CommonIsClickAt(1750, 550, 225, 225) && ((DialogFocusItem.Property.Restrain == null) || (DialogFocusItem.Property.Restrain != "Full"))) InventoryItemMiscBondageBenchStrapsSetPose("Full");
 }
 
 // Sets the cuffs pose (wrist, elbow, both or none)
-function InventoryItemAddonBondageBenchStrapsSetPose(NewPose) {
+function InventoryItemMiscBondageBenchStrapsSetPose(NewPose) {
 	// Gets the current item and character
 	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
 	if ((CurrentScreen == "ChatRoom") || (DialogFocusItem == null)) {
 		DialogFocusItem = InventoryGet(C, C.FocusGroup.Name);
-		InventoryItemAddonBondageBenchStrapsLoad();
+		InventoryItemMiscBondageBenchStrapsLoad();
 	}
 
-	if ((NewPose == null) || (InventoryGet(C, "Cloth") == null) && (InventoryGet(C, "ClothLower") == null)) {
-		if (NewPose == null) {
-			delete DialogFocusItem.Property.Difficulty;
-			delete DialogFocusItem.Property.Type;
-		} else {
-			DialogFocusItem.Property = {SetPose: ["LegsClosed"], Type: NewPose};
-			if (NewPose == "Normal") DialogFocusItem.Property.Difficulty = 3;
-			if (NewPose == "Heavy") DialogFocusItem.Property.Difficulty = 6;
-			if (NewPose == "Full") DialogFocusItem.Property.Difficulty = 9;
-		}
-	} else {
-		InventoryItemAddonBondageBenchStrapsMessage = "RemoveClothesForItem";
+	if (InventoryGet(C, "Cloth") == null || InventoryGet(C, "ClothLower") == null) {
+		InventoryItemMiscBondageBenchStrapsMessage = "RemoveClothesForItem";
 		return;
+	}
+	
+	if (NewPose == null) {
+		delete DialogFocusItem.Property.Difficulty;
+		delete DialogFocusItem.Property.Type;
+	} else {
+		DialogFocusItem.Property.SetPose = ["LegsClosed"];
+		DialogFocusItem.Property.Type = NewPose;
+		if (NewPose == "Normal") DialogFocusItem.Property.Difficulty = 3;
+		if (NewPose == "Heavy") DialogFocusItem.Property.Difficulty = 6;
+		if (NewPose == "Full") DialogFocusItem.Property.Difficulty = 9;
 	}
 	DialogFocusItem.Property.Restrain = NewPose;
 
